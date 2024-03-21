fd7eed8c22e3e3780f8272f3e050bcabf8dbf6c9
Fernando Aponte <fernandoaponte90@gmail.com>
2024-03-21 10:51:40 -0400

    final js

---
+++ js/main.js
@@ -0,0 +1,292 @@
+
+
+const data = {
+    "frases": [
+        {
+            "id": 1,
+            "texto": "Estas flores amarillas son un reflejo de la luz que traes a mi vida. Gracias por iluminar cada día.",
+            "autor": null
+        },
+        {
+            "id": 2,
+            "texto": "Así como estas flores amarillas, nuestra relación está llena de vitalidad, brillo y amor.",
+            "autor": null
+        },
+        {
+            "id": 3,
+            "texto": "Cada flor amarilla es un recordatorio de la felicidad que traes a mi corazón. Eres mi sol.",
+            "autor": null
+        },
+        {
+            "id": 4,
+            "texto": "En cada pétalo de estas flores hay un pensamiento, un deseo y un te amo. Eres mi inspiración.",
+            "autor": null
+        },
+        {
+            "id": 5,
+            "texto": "Estas flores amarillas representan nuestro camino juntos, lleno de alegría, amor y complicidad. Te amo.",
+            "autor": null
+        },
+        {
+            "id": 6,
+            "texto": "Así como estas flores amarillas son vibrantes y llenas de vida, así es mi amor por ti: intenso y eterno.",
+            "autor": null
+        },
+        {
+            "id": 7,
+            "texto": "Estas flores amarillas son un recordatorio de que nuestro amor florece y crece con cada día que pasa.",
+            "autor": null
+        },
+        {
+            "id": 8,
+            "texto": "En cada pétalo de estas flores hay un deseo: que nuestra historia de amor perdure para siempre.",
+            "autor": null
+        },
+        {
+            "id": 9,
+            "texto": "Estas flores amarillas son como el sol en un día nublado, iluminando mi corazón y alma con tu amor.",
+            "autor": null
+        },
+        {
+            "id": 10,
+            "texto": "Cada flor amarilla es un recuerdo de nuestra historia, un momento especial que atesoro en mi corazón.",
+            "autor": null
+        },
+        {
+            "id": 11,
+            "texto": "El amarillo de estas flores simboliza la felicidad que traes a mi vida. Gracias por hacer cada día más brillante.",
+            "autor": null
+        },
+        {
+            "id": 12,
+            "texto": "El amor es la respuesta, y con seguridad, lo sabes. El amor es una flor, tienes que dejarlo crecer.",
+            "autor": "John Lennon"
+        },
+        {
+            "id": 13,
+            "texto": "El amor es como una flor hermosa que no puedo tocar, pero cuya fragancia hace que, igualmente, el jardín sea un lugar de deleite.",
+            "autor": "Rabindranath Tagore"
+        },
+        {
+            "id": 14,
+            "texto": "Los hombres no logran decir “me he equivocado”, ellos envían flores.",
+            "autor": "Joan Manuel Serrat"
+        },
+        {
+            "id": 15,
+            "texto": "Tu amor es mi sol, iluminando cada día de mi vida, como estas hermosas flores amarillas.",
+            "autor": null
+        },
+        {
+            "id": 16,
+            "texto": "Así como estas flores amarillas, tu amor trae luz y alegría a mi corazón en cada momento.",
+            "autor": null
+        },
+        {
+            "id": 17,
+            "texto": "En cada pétalo de esta flor amarilla, hay un pedacito de mi amor por ti.",
+            "autor": null
+        },
+        {
+            "id": 18,
+            "texto": "Al igual que estas flores amarillas, nuestro amor siempre está en flor, radiante y lleno de vida.",
+            "autor": null
+        },
+        {
+            "id": 19,
+            "texto": "El amarillo de estas flores es el color de la felicidad que siento a tu lado.",
+            "autor": null
+        },
+        {
+            "id": 20,
+            "texto": "Estas flores amarillas son como tu luz en mi vida: brillantes, cálidas y llenas de amor.",
+            "autor": null
+        },
+        {
+            "id": 21,
+            "texto": "Así como estas flores iluminan la habitación, tu amor ilumina mi corazón.",
+            "autor": null
+        },
+        {
+            "id": 22,
+            "texto": "Cada pétalo amarillo es un reflejo de la alegría que traes a mi vida. Te amo.",
+            "autor": null
+        },
+        {
+            "id": 23,
+            "texto": "Al igual que estas flores amarillas, nuestro amor es eterno y siempre floreciente.",
+            "autor": null
+        },
+        {
+            "id": 24,
+            "texto": "El amarillo de estas flores es el color de la felicidad que siento a tu lado.",
+            "autor": null
+        },
+        {
+            "id": 25,
+            "texto": "Estas flores amarillas son un símbolo de nuestra felicidad y amor compartido.",
+            "autor": null
+        },
+        {
+            "id": 26,
+            "texto": "Como estas flores amarillas, nuestro amor brilla en cada momento que compartimos.",
+            "autor": null
+        },
+        {
+            "id": 27,
+            "texto": "Tus abrazos son como estas flores amarillas: cálidos, reconfortantes y llenos de amor.",
+            "autor": null
+        },
+        {
+            "id": 28,
+            "texto": "Cada flor amarilla representa un recuerdo especial que compartimos juntos. Te amo.",
+            "autor": null
+        },
+        {
+            "id": 29,
+            "texto": "Una rosa jamás podrá ser un girasol, y un girasol nunca podrá ser una rosa. Cada flor es bella a su manera. Lo mismo pasa con las mujeres.",
+            "autor": null
+        },
+        {
+            "id": 30,
+            "texto": "He mirado a las rosas y me he acordado de ti.",
+            "autor": null
+        },
+        {
+            "id": 31,
+            "texto": "Dame aromas a la salida del Sol, un jardín de hermosas flores donde pueda caminar sin ser molestado.",
+            "autor": null
+        },
+        {
+            "id": 32,
+            "texto": "No tengo ninguna preferencia entre las flores, siempre y cuando sean salvajes, libres y espontáneas.",
+            "autor": null
+        },
+        {
+            "id": 33,
+            "texto": "Cuando se abre una flor, es primavera en todo el mundo.",
+            "autor": null
+        },
+        {
+            "id": 34,
+            "texto": "Siempre hay flores para aquellos que desean verlas.",
+            "autor": null
+        },
+        {
+            "id": 35,
+            "texto": "El amor espera en el borde de un pétalo.",
+            "autor": null
+        },
+        {
+            "id": 36,
+            "texto": "Una de las cosas más atractivas de las flores es su hermosa reserva.",
+            "autor": null
+        },
+        {
+            "id": 37,
+            "texto": "Un hombre nunca es tan grande como cuando se agacha para cuidar una flor o una planta.",
+            "autor": null
+        },
+        {
+            "id": 38,
+            "texto": "Las flores son los ojos con que mirar la naturaleza.",
+            "autor": null
+        },
+        {
+            "id": 39,
+            "texto": "Las flores son la risa de la tierra.",
+            "autor": "Khalil Gibran"
+        },
+        {
+            "id": 40,
+            "texto": "La felicidad está en las flores, cada mañana nos regalan su sonrisa.",
+            "autor": "Anónimo"
+        }
+    ]
+}
+const fraseElement = document.getElementById("frase"); // Reemplaza "frase" con el ID del elemento HTML.
+const autorElement = document.getElementById("autor"); // Reemplaza "autor" con el ID del elemento HTML donde se mostrará el autor.
+
+function mostrarFrase(frase) {
+    fraseElement.textContent = frase.texto;
+
+    // Si la frase tiene autor, mostrarlo.
+    if (frase.autor) {
+        autorElement.textContent = `- ${frase.autor}`;
+    }else{
+        autorElement.textContent = "";
+    }
+}
+
+function cambiarFrase() {
+    // Obtenemos una frase aleatoria del array.
+    const fraseAleatoria = data.frases[getRandomNumber(1, data.frases.length-1)];
+
+    // Mostramos la frase aleatoria.
+    mostrarFrase(fraseAleatoria);
+}
+cambiarFrase(); // Mostramos una frase al cargar la página.
+// Invocamos la función "cambiarFrase" cada 5 segundos.
+setInterval(cambiarFrase, 5000);
+
+
+const container = document.querySelector(".container");
+function createFlower() {
+    const flower = document.createElement("div");
+    flower.classList.add("flower");
+
+    // Create inner elements for the flower
+    const mid = document.createElement("div");
+    mid.classList.add("mid");
+    flower.appendChild(mid);
+
+    // Create petals using the reusable function
+    createPetals(flower, "Petal1", 4);
+    createPetals(flower, "Petal2", 4);
+    createPetals(flower, "Petal3", 4);
+
+    flower.style.left = `${getRandomNumber(-10, 80)}vw`;
+    container.appendChild(flower);
+}
+function createPetals(flower, className, count) {
+    for (let i = 1; i <= count; i++) {
+        const petal = document.createElement("div");
+        petal.classList.add(className, `p${i}`);
+        flower.appendChild(petal);
+    }
+}
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function createRandomFlowers() {
+    const numFlowers = getRandomNumber(2, 10);
+    for (let i = 0; i < numFlowers; i++) {
+        createFlower();
+    }
+}
+function removeAllFlowers() {
+    const flowers = document.querySelectorAll(".flower");
+    for (const flower of flowers) {
+        flower.parentNode.removeChild(flower);
+    }
+}
+function removeHalfFlowers() {
+    const container = document.querySelector(".container");
+    const flowers = container.querySelectorAll(".flower");
+    const numFlowersToRemove = Math.floor(flowers.length / 2);
+
+    // Eliminar la mitad de las flores
+    for (let i = 0; i < numFlowersToRemove; i++) {
+        flowers[i].parentNode.removeChild(flowers[i]);
+    }
+}
+
+setInterval(() => {
+
+    createRandomFlowers();
+}, 7000);
+
+setInterval(() => {
+
+    removeHalfFlowers();
+}, 60000);
